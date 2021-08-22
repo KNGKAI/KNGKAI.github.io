4bfbd6ccd8b02e4ba3d5a27f4447a83166f66a2d
Niel Jordaan <nielj@imrsnt.com>
2021-08-22 17:12:07 +0200

    maybe final

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "eceb3c3d852dc88442baa0107ebd793d",
-"index.html": "cf635a519bc5e426f4ec84698390e172",
-"/": "cf635a519bc5e426f4ec84698390e172",
-"main.dart.js": "0e9b9b589ecae9fae8e5c06dfb876ee4",
+"index.html": "559e8ee2309f74c10d63f127240c8556",
+"/": "559e8ee2309f74c10d63f127240c8556",
+"main.dart.js": "ac17d30eeba39a3c8201a6df15ca6ff9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
